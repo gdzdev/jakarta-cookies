17102a908fa139dd10f8ce49e26837a3c2deb708
aleksGdz <aleksgaldamez2002@gmail.com>
2024-07-09 09:31:34 -0600

    updates

---
+++ src/main/java/org/agaldamez/cookies/service/LoginService.java
@@ -0,0 +1,10 @@
+package org.agaldamez.cookies.service;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.Optional;
+
+public interface LoginService {
+
+    Optional<String> getUsername(HttpServletRequest request);
+}
